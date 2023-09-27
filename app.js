b2ad89c7cfb7a54adbd89d8272665394711fbe01
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-09-27 10:24:53 -0700

    assigment-js-1

---
+++ app.js
@@ -0,0 +1,20 @@
+// alert("");
+// alert("Error! Please enter a valid password");
+// alert("Welcome to JS land  \n  Happy Coding ");
+// alert("Welcome to JS Land");
+// alert("Happy Coding" + + "Prevent this page from creating additional dialogue ");
+// alert("Hello... i can run JS through my web browser console");
+// var headuser = prompt("enter heads username");
+// var tailuser = prompt("enter tails username");
+// var toss = math.random() * 2 ;
+// var floor = math.floor(toss);
+// if (floor === 0 ){ alert ("heads" + headuser +"win the toss")
+// }
+// else{
+//     alert("tails" + tailuser + "win the toss")
+// }
+
+
+
+
+
